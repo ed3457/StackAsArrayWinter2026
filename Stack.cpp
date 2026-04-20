80df264191eba831f3d49405f7af59139cbd0bfe
Kamel R <kamel.r@wayne.edu>
2026-04-20 18:47:33 -0400

    added functions for the stack

--- Stack.cpp
+++ Stack.cpp
@@ -1,5 +1,37 @@
 #include "Stack.h"
 
+void Stack::push(int element)
+{   
+	if (TopIndex + 1 == maxSize)
+		throw 1; // overflow 
+	
+	data[++TopIndex] = element; 
+
+}
+
+int Stack::pop()
+{
+	if (TopIndex == -1)
+		throw 2;// underflow 
+
+	return data[TopIndex--];
+}
+
+int Stack::top()
+{
+	if (TopIndex == -1)
+		throw 2;// underflow 
+
+
+	return data[TopIndex];
+
+}
+
+int Stack::count()
+{
+	return TopIndex+1;
+}
+
 Stack::Stack():Stack(10)
 {
 
@@ -12,4 +44,9 @@ Stack::Stack(int maxSize)
 	TopIndex = -1;
 
 	data = new int[maxSize]; 
+}
+
+Stack::~Stack()
+{
+	delete[] data; 
 }
